dcbef5107ec2b4de8edc26b04db2fbdc383d1fea
jossuema <mmalla1@outlook.com>
2024-08-07 11:50:12 -0500

    miniCNC materials

---
+++ miniCNC/pruebaServo/pruebaServo.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>                    // Incluir la librería Servo
+Servo servo1;                         // Crear un objeto tipo Servo llamado servo1
+int angulo = 0 ;
+
+void setup(){
+  Serial.begin(9600);     // inicia la comunicación serial
+  servo1.attach(8) ;             // Conectar servo1 al pin 8
+}
+
+void loop(){
+  for(angulo  = 20; angulo  <= 50; angulo  += 1){
+    servo1.write(angulo);
+    Serial.print("ANGULO: ");
+    Serial.println(angulo);
+    delay(10);
+  }
+  delay(250);
+  for(angulo  = 50; angulo  >=20; angulo  -=1 ){
+    servo1.write( angulo );
+    Serial.print("ANGULO: ");
+    Serial.println(angulo);
+    delay(10);
+  }
+  delay(250);
+}
